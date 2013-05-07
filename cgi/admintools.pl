0a21d82204cd2c4f15ee1b42b75614a98f844b61
Andrew John Spillman <ajspillm@silo.cs.indiana.edu>
2013-05-07 05:05:03 -0400

    heavily modified admintools.pl

--- cgi/admintools.pl
+++ cgi/admintools.pl
@@ -14,60 +14,115 @@ use DatabaseUtil;
 use HTMLGenerate;
 
 ##load session if logged in
-my $session = CGI::Session->new('driver:sqlite',undef,
-    {Handle=>$DatabaseUtil::sessions_dbh}) or die (CGI::Session->errstr);
 
-our $username = $session->param('username') // "";
-
-my ($path_to_here, $filename) = (url(-full=>1) =~ /(.+)(\/.+\.pl)/);
-sub roster_update_display(){
-  print start_multipart_form();
-  print filefield(-name=>'roster_405',
-      -size=>50,
-      -maxlength=>400).submit().end_multipart_form();
+our $dbdir='../db';
+use DBI;
 
 
-# undef may be returned if it's not a valid file handle
-  my $roster_405_fh  = upload('roster_405');
-  if (defined $roster_405_fh) {
-# Upgrade the handle to one compatible with IO::Handle:
-    my $io_handle = $roster_405_fh->handle;
-    open (OUTFILE,">>","$Bin/temp");
-    while (my $I = $io_handle->read(my $buffer,1024)) {
-      print OUTFILE $buffer;
-    };
-#open (INFILE,'<',"$Bin/../temp");
-#print INFILE;
-close OUTFILE or die;
-#Delete() or die;
-#undef $io_handle or die;
-  };
-};
-print $session->header(-expires=>'now');
-print start_html(-title=>'DRA Studio Sign out sheet',
-    -style=>{'src'=>'/room/draStudioSched.css'},
-    -head=>meta({-http_equiv => 'Content-Language',
-      -content => 'en'}));
-    if (!$username) { 
-      print a({-href=>'cas_basic.cgi'}, "CAS Login");
-    } else{
-      print p("Currently logged in as ".DatabaseUtil::getrealname($username)).
-        a({-href=>url(-base=>1)."/room/cgi-bin/Logout.pl"}, "Logout");
+## undef may be returned if it's not a valid file handle
+#      my $roster_405_fh  = upload('roster_405');
+#      if (defined $roster_405_fh) {
+## Upgrade the handle to one compatible with IO::Handle:
+#      my $io_handle = $roster_405_fh->handle;
+#      open (OUTFILE,">>","$Bin/temp");
+#      while (my $I = $io_handle->read(my $buffer,1024)) {
+#      print OUTFILE $buffer;
+#
+#      };
+##open (INFILE,'<',"$Bin/../temp");
+##print INFILE;
+#      close OUTFILE or die;
+##Delete() or die;
+##undef $io_handle or die;
+# 
+
+#database connection for CGI Session
+  my $session = CGI::Session->new('driver:sqlite',undef,
+      {Handle=>$DatabaseUtil::sessions_dbh}) or die (CGI::Session->errstr);
+  our $username = $session->param('username') // "";
+  my ($path_to_here, $filename) = (url(-full=>1) =~ /(.+)(\/.+\.pl)/);
+    my ($sth,@names,$row,$timeblock_length,$visible);
+  print $session->header(-expires=>'now');
+  print start_html(-title=>'DRA Studio Sign out sheet',
+      -style=>{'src'=>'/room/draStudioSched.css'},
+      -head=>meta({-http_equiv => 'Content-Language',
+        -content => 'en'}));
+  if (!$username) { 
+    print a({-href=>'cas_basic.cgi'}, "CAS Login");
+  } else{
+    print p("Currently logged in as ".DatabaseUtil::getrealname($username)).
+      a({-href=>"$path_to_here/Logout.pl"}, "Logout");
   }; 
 #my $faculty = DatabaseUtil::isfaculty($username) unless !$username;
-my $faculty = 1;
-if($faculty){
+  my $faculty = 1;
+  if($faculty){
 
-roster_update_display();
+#database connection for config
+    my $config_dbh = DBI->connect("dbi:SQLite:dbname=$dbdir/config.db",
+        "{RaiseError => 1}","")or die;
+    $config_dbh->do('CREATE TABLE IF NOT EXISTS rooms (name TEXT DEFAULT new
+          PRIMARY KEY, timeblock_length TEXT DEFAULT \'2 hours\', visible TEXT
+          DEFAULT 1)');
+    if(param('which_room')){
+      if(param('delete')){
+          $sth = $config_dbh->prepare('DELETE FROM rooms WHERE name=?');
+          $sth->execute(param('which_room'));
+          }else{
+          $sth = $config_dbh->prepare('UPDATE rooms SET timeblock_length=?,
+            visible=? WHERE name=?');
+          $sth->execute(param('timeblock_length'),(param('visible')? 1 : 0),
+            param('which_room'));
+          print ("hey".param('timeblock_length').(param('visible')? 1 : 0));
+          };
+          Delete_all();
+          }elsif(param('new_room')){
+          $sth = $config_dbh->prepare('INSERT INTO rooms (name) VALUES (?)');
+          $sth->execute(param('new_room'));
+          Delete_all();
+          }; 
 
-}else{
-  print p("You don't appear to be faculty. Try logging out and revist this page.").
- a({-href=>url(-base=>1)."/room/cgi-bin/Logout.pl"}, "Logout");
-};
+#generate array of room titles
+   $sth = $config_dbh->prepare('SELECT name from rooms');
+    $sth->execute();
+    $sth->bind_col(1,\$row);
+    while ($sth->fetch()){
+      push (@names, $row);
+    }
 
+    print h1("Schedule Options:");
+    foreach my $room_name (@names){
+      $sth = $config_dbh->prepare("SELECT timeblock_length, visible  from rooms where name=?");
+      $sth->execute($room_name);
+      $sth->bind_columns(\$timeblock_length,\$visible);
+      $sth->fetch();
+      print $timeblock_length.$visible; 
+      print h2("$room_name Configuration").
+        start_multipart_form().
+        span("Length of time blocks:").
+        popup_menu('timeblock_length',['1 hour','2 hours','4 hours'],
+            $timeblock_length).
+        span("roster upload:").
+        filefield(-name=>"roster", -size=>50, -maxlength=>400).
+        checkbox(-name=>'visible',-checked=>$visible,-value=>1,
+            -label=>"Show $room_name?").
+        checkbox(-name=>'delete',checked=>0,-value=>1,
+            -label=>"Delete this room?").
+        hidden(-name=>'which_room',-value=>$room_name).        
+        submit().
+        end_multipart_form();
+      print'<BR><BR>';
 
+    }
+    print "<BR><BR>.start_form().
+     h1("Create new schedule.").
+      textfield(-name=>'new_room',-value=>'new-schedule-name',-size=>20,
+          -maxlength=>20).
+      submit(-value=>'Create Schedule').
+      end_form();
 
-
-
-print end_html();
+  }else{
+    print p("You don't appear to be faculty. Try logging out and revist this page.").
+      a({-href=>url(-base=>1)."/room/cgi-bin/Logout.pl"}, "Logout");
+  };
+  print end_html();
 
--- lib/excelparse.pl
+++ lib/excelparse.pl
@@ -9,13 +9,19 @@ use Spreadsheet::ParseExcel;
 # in next row.  If roster is exported from Oncourse in different format,
 # change the following values, furthest left column is 0.
 
+package ParseExcel;
+
+
+
+
+sub makeroster{
+
+
 
 my $realname_col = 0;   #column of Real Name
 my $username_col = 1;   #column of User ID
 my $role_col = 3;       #column of Role
 my $first_data_row = 1;  #first row containing useful data
-
-
 
 my $dbfile="test.db";
 my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","{RaiseError => 1}","");
@@ -30,21 +36,16 @@ my $worksheet =$workbook->worksheet(0);
 
 my @row_range = $worksheet->row_range();
 
-  for(my $iR = 1; $iR <= $row_range[1]; $iR++) {
-    $username = $worksheet->get_cell($iR, $username_col)->value();
-my $name = $worksheet->get_cell($iR, $realname_col)->value();
+for(my $iR = 1; $iR <= $row_range[1]; $iR++) {
+  $username = $worksheet->get_cell($iR, $username_col)->value();
+  my $name = $worksheet->get_cell($iR, $realname_col)->value();
+  $name =~ /(\w*),\s(\w*)\s?/;
+  $realname =  $2." ".$1; 
+# ^ "Lastname, First Middle" -> "First Lastname"
+  print ("\"".$realname. "\"\n "); 
 
-
-
-$name =~ /(\w*),\s(\w*)\s?/;
-    $realname =  $2." ".$1; 
-              # ^ "Lastname, First Middle" -> "First Lastname"
-print ("\"".$realname. "\"\n "); 
-
-    $role = $worksheet->get_cell($iR, $role_col)->value();
-    $sth->execute($username, $realname, $role);
+  $role = $worksheet->get_cell($iR, $role_col)->value();
+  $sth->execute($username, $realname, $role);
 
   print "added $username, $realname, $role";
-  }
-
-
+}
